ae3ed16790210c84c7134366a47e34f6c56aed76
debbili <liyuexuan25@163.com>
2017-02-28 23:56:26 +0800

    Add create note activity(temp)

---
+++ Note/src/main/java/debbili/studio/debbilinote/note/CreateNoteActivity.java
@@ -0,0 +1,15 @@
+package debbili.studio.debbilinote.note;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import debbili.studio.debbilinote.R;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+    }
+}
